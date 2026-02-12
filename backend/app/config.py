9d434b2143fe2798265e064896708f952eb72136
muskiteer <agarwal.vansh3110@gmail.com>
2026-02-12 23:53:14 +0530

    fixed depl

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 
-// setting up the contract
+# setting up the contract
 
 class Settings(BaseSettings):
     database_url: str
